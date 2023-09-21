809c4d8cdb1f483306ddf38624f47e16e6ee6b83
MariiaZhvanko <zhvmr09@gmail.com>
2023-09-21 21:04:11 +0300

    mocked the price feed contract and modified main contract to test with the mock

--- contracts/Bull&Bear.sol
+++ contracts/Bull&Bear.sol
@@ -19,9 +19,12 @@ contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, KeeperCompatibl
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    AggregatorV3Interface public pricefeed;
 
     uint public /* immutable */ interval;
     uint public lastTimeStamp;
+
+    int256 public currentPrice;
 
     // IPFS URIs for the dynamic nft graphics/metadata.
     string[] bullUrisIpfs = [
@@ -35,9 +38,24 @@ contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, KeeperCompatibl
         "https://ipfs.io/ipfs/QmbKhBXVWmwrYsTPFYfroR2N7NAekAMxHUVg2CWks7i9qj?filename=simple_bear.json"
     ];
 
-    constructor(uint updateInterval) ERC721("Bull&Bear", "BBTK") {
+    constructor(uint updateInterval, address _pricefeed) ERC721("Bull&Bear", "BBTK") {
         interval = updateInterval;
         lastTimeStamp = block.timestamp;
+
+        pricefeed = AggregatorV3Interface(_pricefeed);
+        currentPrice = getLatestPrice();
+    }
+
+    function getLatestPrice() public view returns (int256) {
+        (
+            /*uint80 roundID*/,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = pricefeed.latestRoundData();
+        
+        return price;
     }
 
     function safeMint(address to) public {
@@ -84,13 +102,14 @@ contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, KeeperCompatibl
         return super.supportsInterface(interfaceId);
     }
 
-    function checkUpkeep(bytes calldata) external view override returns (bool upkeepNeeded, bytes memory) {
-        upkeepNeeded = (block.timestamp -lastTimeStamp) > interval;
+    function checkUpkeep(bytes calldata /* checkData */) external view override returns (bool upkeepNeeded, bytes memory /* performData */) {
+        upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
     }
 
-    function performUpkeep(bytes calldata) external override {
+    function performUpkeep(bytes calldata /* performData */) external override {
         if ((block.timestamp - lastTimeStamp) > interval) {
             lastTimeStamp = block.timestamp;
+            currentPrice = getLatestPrice();
             console.log('performing upkeep');
         } else {
             console.log('interval is not up yet');
